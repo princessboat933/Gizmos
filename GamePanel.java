6036751348281a08b1b5b093047903bbd443a479
mer <150179077+CoderissaTheMermaid@users.noreply.github.com>
2023-11-10 21:09:21 -0600

    Game Panel UPDATED
    - placeholders for all the possible items on the screen
    - I will replace the placeholders with images once we have all the object classes

--- GamePanel.java
+++ GamePanel.java
@@ -1,4 +1,5 @@
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
@@ -19,7 +20,7 @@ import javax.swing.JPanel;
  * pick1: (256, 750) - drawImage
  */
 
-public class GamePanel extends JPanel implements MouseListener
+public class GamePanel extends JPanel implements MouseListener 
 {
 	BufferedImage bg;
 	public GamePanel()
@@ -38,26 +39,162 @@ public class GamePanel extends JPanel implements MouseListener
 	
 	public void paint(Graphics g)
 	{
+		//board background 
 		g.drawImage(bg, 0, 0, getWidth(), getHeight(), null);
 		g.setColor(Color.cyan);
-		g.fillOval(256,750, 35, 35);
-		int x = 256;
+		int x = 250;
 		int y = 750;
 		
+		//marbles
 		for(int i=0; i<6; i++)
 		{
 			g.fillOval(x,y, 35, 35);
 			x+=16;
 			y-=31;
 		}
-		x=559;
-		y=910;
-		g.fillOval(559,910, 35, 35);
-		g.fillOval(598,920, 35, 35);
-		g.fillOval(592,958, 35, 35);
-		g.fillOval(557,948, 35, 35);
-		g.fillOval(517,958, 35, 35);
-		g.fillOval(517,920, 35, 35);
+		
+		
+		x = 515;
+		
+		y = 906;
+		for(int j=0; j<4; j++) 
+		{	
+			int temp = x;
+			y=906;
+			for(int i=0; i<3; i++)
+			{
+				
+					g.fillOval(x,y, 35, 35);
+					g.fillOval(x,y+40, 35, 35);
+					
+					
+					x+=40;
+					if(i==0)
+					{
+						y-=10;
+					}
+					else if(i==1)
+					{
+						y+=10;
+					}
+					
+			}
+			
+			if(j==1)
+			x=temp+370;
+			
+			else
+			{
+				x=temp+345;
+			}
+		}
+		
+		
+	
+		//tier cards: size 83x83
+		x=35;
+		y=119;
+		for(int i=0; i<4; i++)
+		{
+			g.fillRect(x, y, 83, 83);
+			y+=90;
+		}
+		x=155;
+		y=119;
+		for(int i=0; i<3; i++)
+		{
+			g.fillRect(x, y, 83, 83);
+			y+=90;
+		}
+		x=274;
+		y=119;
+		for(int i=0; i<2; i++)
+		{
+			g.fillRect(x, y, 83, 83);
+			y+=90;
+		}
+		
+		//player cards
+		//loops through each player's cards
+		x=427;
+		y=58;
+		for(int p=0; p<2; p++)
+		{
+			for(int i=0; i<6; i++)
+			{
+				
+				if(i==2)
+				{
+					y+=20;
+				}
+				if(i==5)
+				{
+					x+=80;
+					y-=30;
+				}
+				for(int j=0; j<15; j++)
+				{
+					
+					g.drawRect(x, y, 83, 83);
+					y+=20;
+				}
+				x+=105;
+				y=58;
+				
+			}
+			
+			x+=30;	
+		}
+		x=427;
+		y=455;
+		for(int p=0; p<2; p++)
+		{
+			for(int i=0; i<6; i++)
+			{
+				
+				if(i==2)
+				{
+					y+=20;
+				}
+				if(i==5)
+				{
+					x+=80;
+					y-=30;
+				}
+				for(int j=0; j<15; j++)
+				{
+					
+					g.drawRect(x, y, 83, 83);
+					y+=20;
+				}
+				x+=105;
+				y=455;
+				
+			}
+			
+			x+=30;	
+		}
+		
+		
+		//scores
+		//loop through all the players 
+		x=763;
+		y=991;
+		for(int i=0; i<4; i++)
+		{
+			
+			g.setFont(new Font("SansSerif", Font.BOLD, 20));
+			g.drawString("0", x, y);
+			if(i==1)
+			{
+				x+=370;
+			}
+			else
+			x+=350;
+		}
+		
+		
+		
 		
 	}
 
