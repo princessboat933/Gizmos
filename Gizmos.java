97bc19d3e07c8be749024fd0e0f450b735a1c422
PlatypusPotatoo <147188047+PlatypusPotatoo@users.noreply.github.com>
2023-11-10 23:30:30 -0600

    Update Gizmos.java

--- Gizmos.java
+++ Gizmos.java
@@ -1,4 +1,60 @@
 
-public class Gizmos {
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import javax.swing.*;
 
+public class Gizmos implements MouseListener{
+	
+	private JPanel[] panelList = new JPanel[2];
+	private int current;
+	private JPanel panels;
+	
+	public Game() {
+		JFrame frame = new JFrame();
+		JPanel p1 = new GamePanel();
+		current = 0;
+		panelList[0] = p1;
+		p1.addMouseListener(this);
+		        
+		panels = new JPanel(new CardLayout());
+		panels.add(p1, "Panel 1");
+
+		Container pane = frame.getContentPane();
+		pane.add(panels, BorderLayout.CENTER);
+		            
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setSize(400, 400);
+		frame.setVisible(true);
+	}
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		int x = e.getX();
+		int y = e.getY();
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
 }
